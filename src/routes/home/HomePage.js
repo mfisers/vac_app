67bc82217979e860c87b3cac1dcbb76ceeba4d81
mfisers <martins.fisers@accenture.com>
2016-12-22 17:23:05 +0200

    Added page content

--- src/routes/home/HomePage.js
+++ src/routes/home/HomePage.js
@@ -9,28 +9,36 @@ class HomePage extends Component {
         <Carousel />
           <div className="app-content-page-text">
             <Link to="/home">
-            <div className="product-container"><span>Product container1</span></div>
+            <span>Produkts 1</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container2</span></div>
+            <span>Produkts 2</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container3</span></div>
+            <span>Produkts 3</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container4</span></div>
+            <span>Produkts 4</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container5</span></div>
+            <span>Produkts 5</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container6</span></div>
+            <span>Produkts 6</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container7</span></div>
+            <span>Produkts 7</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
             <Link to="/home">
-            <div className="product-container"><span>Product container8</span></div>
+            <span>Produkts 8</span>
+            <img src="http://larics.rasip.fer.hr/wp-content/uploads/2016/04/default-placeholder.png"/>
             </Link>
           </div>
       </div>
